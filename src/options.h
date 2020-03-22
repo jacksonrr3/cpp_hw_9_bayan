3654b6f9da56929638f3e55c2fde4b335111e4c2
jacksonrr3 <49640784+jacksonrr3@users.noreply.github.com>
2020-03-22 11:11:57 +0300

    Update options.h

--- src/options.h
+++ src/options.h
@@ -3,6 +3,8 @@
 #include <vector>
 #include <string>
 #include <boost/program_options.hpp>
+#include <boost/format.hpp>
+//#include <boost/format.hpp>
 
 #include "hash.h"
 
@@ -11,7 +13,8 @@ using namespace boost::program_options;
 class Options {
 	options_description _desc{ "Options" };
 	variables_map _vm;
-
+	std::string _path;
+	std::string _type;
 	std::vector<std::string> _files;
 	std::size_t _block_size;
 	std::string _hash_type;
@@ -21,18 +24,25 @@ public:
 
 	Options() {
 		_desc.add_options()
-			("help,h", "Help screen")
+			("help", "Help screen")
+			("path", value<std::string>(), "path to files")
+			("type", value<std::string>(), "file type")
 			("files", value<std::vector<std::string>>()->
-				multitoken()->zero_tokens()->composing(), "файлы для сканирования")
-			("size", value<std::size_t>()->default_value(0), "размер блока для чтения файлов")
-			("hash", value<std::string>()->default_value(""), "алгоритм хэширования");
+				multitoken()->zero_tokens()->composing(), "file names for compare")
+			("size", value<std::size_t>()->default_value(3), "block size")
+			("hash", value<std::string>()->default_value("boost_hash"), "hash type (boost_hash or boost_crc32)");
+		
 	}
 
 	void parse_comm_line_opt(int argc, const char* argv[]) {
 		store(parse_command_line(argc, argv, _desc), _vm);
-		
+		notify(_vm);
+
+		if (_vm.count("help"))
+			std::cout << _desc << '\n';
+
 		if (_vm.count("size")) {
-			_block_size = _vm["size"].as<std::size_t>(); 
+			_block_size = _vm["size"].as<std::size_t>();
 		}
 
 		if (_vm.count("hash")) {
@@ -41,12 +51,23 @@ public:
 			_hash_type = _vm["hash"].as<std::string>();
 		}
 
-		if (_vm.count("files")) {
-			_files = _vm["files"].as<std::vector<std::string>>();
+		if (_vm.count("path")) {
+			_path = _vm["path"].as<std::string>();
 		}
 
-		if ((_files.size() == 0) || (_hash_type == "") || (_block_size == 0)) {
-			throw Bayan_exeption("wrong_data!");
+		if (_vm.count("type")) {
+			_type = "." + _vm["type"].as<std::string>();
+		}
+
+		if (_vm.count("files")) {
+			_files = _vm["files"].as<std::vector<std::string>>();
+			for (auto& f : _files) {
+				f = _path + f + _type;
+			}
+		}
+
+		if ((_files.size() == 0)) {
+			std::cout << "No files for compare" << std::endl;
 		}
 	}
 
